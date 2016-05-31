7fd4c549699ebc2c95e50ed2d0cf528449e53dae
Joshua <rockerjman222@rogers.com>
2016-05-31 10:09:00 -0400

    Loading maps should technically be done...

--- src/me/rockerjman222/Lttp/map/Map.java
+++ src/me/rockerjman222/Lttp/map/Map.java
@@ -1,18 +1,17 @@
 package me.rockerjman222.Lttp.map;
 
-import org.lwjgl.system.libffi.Closure;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import sun.misc.BASE64Decoder;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Map {
 
@@ -21,6 +20,7 @@ public class Map {
 	private int tileWidth;
 	private int tileHeight;
 
+	private HashMap<Integer, Integer> tiles = new HashMap<>();
 
 	public Map() {
 
@@ -33,18 +33,24 @@ public class Map {
 			Document doc = dBuilder.parse(file);
 			doc.getDocumentElement().normalize();
 
-			System.out.println(doc.getDocumentElement().getNodeName());
-
 			NodeList layerNodes = doc.getElementsByTagName("layer");
 			NodeList tileSetNodes = doc.getElementsByTagName("tileset");
+			NodeList dataNodes = doc.getElementsByTagName("tile");
 			Element eLayer = (Element) layerNodes.item(0);
 			Element eTileSets = (Element) tileSetNodes.item(0);
+
+			for(int i = 0; i < dataNodes.getLength(); i++) {
+				Node tile = dataNodes.item(i);
+				if(tile.getNodeType() == Node.ELEMENT_NODE) {
+					Element eTile = (Element) tile;
+					this.tiles.put(i, Integer.parseInt(eTile.getAttribute("gid")));
+				}
+			}
 
 			this.mapWidth = Integer.parseInt(eLayer.getAttribute("width"));
 			this.mapHeight = Integer.parseInt(eLayer.getAttribute("height"));
 			this.tileWidth = Integer.parseInt(eTileSets.getAttribute("tilewidth"));
 			this.tileHeight = Integer.parseInt(eTileSets.getAttribute("tileheight"));
-
 
 
 		} catch (ParserConfigurationException | SAXException | IOException e) {
